d9e99113a47d48a83a7c4dad6de398e3853643e3
Bailey Cross <59812673+baileyfcross@users.noreply.github.com>
2022-02-17 15:27:53 -0500

    Add files via upload

---
+++ maker.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+/**
+ * Write a description of class maker here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class maker
+{
+    public static void main(String args[]){
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        int x = rand.nextInt(2);
+        while(!sc.nextLine().equals("q")){
+            x = rand.nextInt(2);
+            if(x == 0){
+                System.out.println("We should get Chipotle");
+            }
+            else{
+                System.out.println("We should get Chinese");
+            }
+        }
+    }
+}
